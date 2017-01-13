dd2c2ca5583ddfc880cde797631c36b9faa79040
Maryam Ali <mali62541@gmail.com>
2017-01-12 22:14:43 -0500

    5.4 users table scaffoled and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161229040711) do
+ActiveRecord::Schema.define(version: 20170113031359) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 20161229040711) do
     t.datetime "updated_at",  null: false
     t.string   "colour"
     t.integer  "price"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
