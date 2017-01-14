c31a2a1000d9e3b4baa7dd0d13af647db6581f70
Maryam Ali <mali62541@gmail.com>
2017-01-13 23:14:01 -0500

    5.4 more devise customization

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,7 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
+  #devise custom for :users
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
