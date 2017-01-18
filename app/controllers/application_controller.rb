fc0ae541d83efd529fc16e41b67cfc7756310031
Maryam Ali <mali62541@gmail.com>
2017-01-17 19:46:56 -0500

    5.4 error customized and notices added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  #provide flash error notice and redirected to home page when trying to edit any user other than the logged in one
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to main_app.root_url, :alert => exception.message
+end
 end
 
