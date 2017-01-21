a98a5df8f94a39a074c7c0f792f97b439d0eb0e6
Maryam Ali <mali62541@gmail.com>
2017-01-20 19:29:02 -0500

    5.7 added scopes for comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :product
+
+  # Scopes
+  scope :rating_desc, -> { order(rating: :desc) }
+  scope :rating_desc, -> { order(rating: :asc) }
 end
