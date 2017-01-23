71668f077ed538dac0bc0b1688f42447f656ec08
Maryam Ali <mali62541@gmail.com>
2017-01-23 01:44:01 -0500

    5.8 no turbolinks

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,9 +1,11 @@
 $(document).on('turbolinks:load', function(){
   $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]'});
+  console.log("rating test");
   $('.rated').raty({ path: '/assets',
     readOnly: true,
     score: function() {
       return $(this).attr('data-score');
     }
   });
+  console.log("rated test");
 });
