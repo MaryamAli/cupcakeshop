984e56ae958d5485305575f68a9875792be5b2df
Maryam Ali <mali62541@gmail.com>
2017-01-26 23:14:00 -0500

    5.10 Admin role working

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,7 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
+    can :destroy, Comment, user.admin: true # allows only admin to delete comments
   end
 
 end
