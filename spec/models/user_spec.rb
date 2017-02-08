c3e5b2e5ea7e24189807542b5a8ac4f2464feabf
Maryam Ali <mali62541@gmail.com>
2017-02-07 21:49:26 -0500

    comments for myself

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-describe User do
+# demo, not necessary, b/c Devise gem already validates users
+
+# describe User, :type => :model do
+#   it "should not validate users without an email address" do
+#     @user = FactoryGirl.build(:user, email: "not_an_email")
+#     expect(@user).to_not be_valid
+#   end
 
 end
