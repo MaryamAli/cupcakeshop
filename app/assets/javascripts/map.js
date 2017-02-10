a7440a6661b2a9233d1fd1e4a32fc6ba10551ea5
Maryam Ali <mali62541@gmail.com>
2017-02-09 21:08:49 -0500

    fix for heroku

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,14 +1,14 @@
-// Google Map
-var map;
-function initMap() {
-  var myLatLng = {lat: 33.8224112, lng: -84.3712244};
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: myLatLng,
-    zoom: 15,
-  });
-  var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    title: 'Hello from Atlanta!'
-  });
-}
+// // Google Map
+// var map;
+// function initMap() {
+//   var myLatLng = {lat: 33.8224112, lng: -84.3712244};
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     center: myLatLng,
+//     zoom: 15,
+//   });
+//   var marker = new google.maps.Marker({
+//     position: myLatLng,
+//     map: map,
+//     title: 'Hello from Atlanta!'
+//   });
+// }
