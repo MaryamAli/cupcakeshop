b5707d20b1f0a72d4bb6c33d5d247996ebb13c39
Maryam Ali <mali62541@gmail.com>
2017-02-21 16:22:52 -0500

    6.14 ActiveJob

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -10,6 +10,9 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
   received: function(data) {
     // Called when there's incoming data on the websocket for this channel
     $(".alert.alert-info").show();
+    $('.product-reviews').prepend(data.comment);
+    $("#average-rating").attr('data-score', data.average_rating);
+    refreshRating();
   },
 
   listen_to_comments: function() {
@@ -23,3 +26,5 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
 $(document).on('turbolinks:load', function() {
   App.product.listen_to_comments();
 });
+
+
